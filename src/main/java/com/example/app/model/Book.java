af69b7f32a284fc6bc76bebef61161da51eee842
Radu <raadned@users.noreply.github.com>
2021-06-19 14:11:17 +0300

    add model class

---
+++ src/main/java/com/example/app/model/Book.java
@@ -0,0 +1,57 @@
+package com.example.app.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int numPages;
+    private LocalDate releaseDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        this.numPages = numPages;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return numPages == book.numPages && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(releaseDate, book.releaseDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, numPages, releaseDate);
+    }
+}
