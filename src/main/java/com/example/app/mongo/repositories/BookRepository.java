6d3d93b67ed9f214fa7e74ff28147c13929e2cd0
Radu <raadned@users.noreply.github.com>
2021-08-02 05:29:14 +0100

    add findById functionality

--- src/main/java/com/example/app/mongo/repositories/BookRepository.java
+++ src/main/java/com/example/app/mongo/repositories/BookRepository.java
@@ -4,7 +4,9 @@ import com.example.app.mongo.documents.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends MongoRepository<Book, String> {
     List<Book> findByTitle(String title);
+    Optional<Book> findById(String id);
 }
